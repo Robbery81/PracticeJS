b122b4e91d449ac0efa9dfc3d9c31c4ea070805c
Aleksandr <alexandr.robbery@gmail.com>
2020-05-16 12:29:10 +0300

    train get elem from html page

---
+++ udemy/2.cinema/js/script.js
@@ -0,0 +1,43 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+let adv = document.querySelectorAll(".promo__adv img");
+adv.forEach(item => item.remove());
+
+document.getElementsByClassName("promo__genre")[0].textContent = "Драма";
+
+document.getElementsByClassName("promo__bg")[0].style.background = "URL('img/bg.jpg') top";
+
+let list = document.querySelector(".promo__interactive-list");
+list.innerHTML = "";
+
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {
+    list.innerHTML += `
+    <li class="promo__interactive-item">${i+1}. ${film}
+        <div class="delete"></div>
+    </li>`
+});

5a1d2220039abca4f2c8258ad9b95a7c5cd0178d
Aleksandr <alexandr.robbery@gmail.com>
2020-06-08 21:40:13 +0300

    timer

--- udemy/3.tab/js/script.js
+++ udemy/3.tab/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    //Tab
     const tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
@@ -34,4 +36,51 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     });
+
+    //Timer
+    const deadline = "2020-06-09T14:00:00";
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (24 * 60 * 60 * 1000)),
+            hours = Math.floor((t / (60 * 60 * 1000)) % 24),
+            min = Math.floor((t / (60 * 1000)) % 60),
+            sec = Math.floor((t / 1000) % 60);
+        return {
+            'total': t,
+            days,
+            hours,
+            min,
+            sec
+        };
+    }
+
+    function setTimeRemaining(selector, endtime) {
+        let timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function getZero(num) {
+            return num >= 10 ? num : `0${num}`;
+        }
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.min);
+            seconds.innerHTML = getZero(t.sec);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setTimeRemaining('.timer', deadline);
 });

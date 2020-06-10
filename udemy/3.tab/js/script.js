fb41cf03d361176a8a4cb75fcb78b580e4c3bd28
Aleksandr <alexandr.robbery@gmail.com>
2020-06-10 14:04:55 +0300

    add dynemic card content

--- udemy/3.tab/js/script.js
+++ udemy/3.tab/js/script.js
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function showModal() {
         modalWindow.style.display = "block";
         document.body.style.overflow = "hidden";
-        clearInterval(modalTimerId);
+        //clearInterval(modalTimerId);
     }
 
     function hideModal() {
@@ -127,8 +127,74 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    const modalTimerId = setTimeout(showModal, 10000);
+    //const modalTimerId = setTimeout(showModal, 10000);
 
     window.addEventListener('scroll', showModalByScroll);
 
+    //Menu
+
+    class MenuItem {
+        constructor(imgUrl, alt, subtitle, descr, total, parentSelector) {
+            this.imgUrl = imgUrl;
+            this.alt = alt;
+            this.subtitle = subtitle;
+            this.descr = descr;
+            this.total = total;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+
+        changeToUAH() {
+            this.total *= this.transfer;
+        }
+
+        render() {
+            let element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+            <img src=${this.imgUrl} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.total}</span> грн/день</div>
+            </div>
+            </div>`;
+            console.log(this.parent);
+            console.log(element);
+            this.parent.append(element);
+        }
+
+    }
+
+    let menuItemContent = {
+        img: ["img/tabs/post.jpg", "img/tabs/elite.jpg", "img/tabs/vegy.jpg"],
+        alt: ["post", "elite", "vegy"],
+        sub: ['Меню "Постное"', 'Меню “Премиум”', 'Меню "Фитнес"'],
+        descr: [
+            "Меню“ Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+            "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+            "Меню “Фитнес” - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!"
+        ],
+        total: [430, 550, 229],
+        parentSelector: ['.menu .container']
+    };
+
+    function createMenuItem(content) {
+        let countItem = content.img.length;
+        console.log("object");
+        for (let i = 0; i < countItem; i++) {
+            new MenuItem(
+                content.img[i],
+                content.alt[i],
+                content.sub[i],
+                content.descr[i],
+                content.total[i],
+                content.parentSelector[0]).render();
+        }
+    }
+    createMenuItem(menuItemContent);
 });
